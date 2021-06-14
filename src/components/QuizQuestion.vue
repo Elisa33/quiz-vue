21f60ab7c7e7ca21e7978cc4732eded3d57fe606
elisa33 <pipi.tdfuego@gmail.com>
2021-06-13 22:19:56 -0400

    quiz with vue

---
+++ src/components/QuizQuestion.vue
@@ -0,0 +1,83 @@
+<template lang="">
+    <div class="container">
+        <div class="title">
+            <h2>Question {{index +1}}/10</h2>
+        </div>
+        <div class="question">{{items[index]['question']}}</div>
+
+        <div  :for="key" v-for="answer,key of items[index]['incorrect_answers']" :key="key">
+            <div class="answer hover" 
+            value="wrong" 
+            @click="answered">
+            {{answer }} 
+            </div>
+         </div>
+        <div class="answer hover" 
+        value="right"
+        @click="answered">
+            {{items[index]['correct_answer'] }}
+        </div>
+        <div class="info">
+            <span class="datum">{{items[index]['category']}}</span>
+            <span class="datum">Type: {{items[index]['type']}}</span>
+            <span class="datum">Difficulty: {{items[index]['difficulty']}}</span>
+        </div>
+        <!-- popup -->
+        <div class="popup" @click="next"  :class="{'reset': popup == 0 }">
+            <div class="check" :class="{'correct': popup == 1, 'incorrect': popup ==2}" v-show="index < 9">{{icon}}</div>
+            <button class="next" v-show="index < 9">Next</button>
+        </div>
+        <div class="finish" v-show="index == 9"><span>Correct Answers:</span><span> {{correctA}}/10</span></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuizQuestion',
+    props:['data'],
+    data(){
+    
+    return{
+      index: 0,
+      selected:'',
+      correctA:0,
+      popup: 0,
+      icon: '',
+      
+      items: this.data
+          }
+  },
+     methods:{
+        unescape(string) {
+            this.icon = document.querySelector(".check");
+            this.icon.innerHTML = string;
+        },
+        answered(e){
+        this.selected = e.target.attributes.value.value;
+
+        if(this.selected == "right"){
+            this.correctA++;
+            this.popup = 1;
+            this.icon = this.unescape("&check;")
+        }else{
+            this.popup = 2;
+            this.icon = this.unescape("&#x2E3;");
+        }
+
+        },
+        popreset(){
+            this.popup = 0;
+        },
+        next(){
+            this.index++;
+            this.popup = 0;
+        }
+  }
+}
+</script>
+
+<style lang="">
+
+
+    
+</style>
---
+++ src/components/QuizTest.vue
@@ -0,0 +1,167 @@
+<template>
+  <div >
+    <QuizQuestion :data="data"/>
+  </div>
+</template>
+
+<script>
+import QuizQuestion from './QuizQuestion.vue'
+import datos from "../data.json"
+export default {
+
+  components: {
+      QuizQuestion
+  },
+  data(){
+    return{
+      data: datos
+          }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #5a5a5a;
+  margin-top: 60px;
+}
+body{
+  background: #efefef;
+
+}
+h1{
+    width: 100%;
+}
+h2{
+    color:#fff;
+}
+.container{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    max-width: 600px;
+    background: #fff;
+    margin: 1rem auto;
+    border-radius: 20px;
+    overflow: hidden;
+}
+@media (max-width: 640px) {
+  .container {
+    max-width: 500px;
+  }
+}
+.title{
+    background: #5acfc9;
+}
+.info{
+    background: #5acfc9;
+    padding: 1rem 2rem;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1.5rem;
+}
+.question{
+  padding: 1.5rem 2rem;
+  font-size: 1.2rem;
+}
+.options{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.answer{
+  background: #fa9f42;
+  padding: 0.8rem 2rem;
+  margin: 0.8rem 2rem;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+}
+.hover:hover{
+    background: #5acfc9;
+}
+.next{
+  background: #5acfc9;
+  padding: 0.5rem 2rem;
+  border-radius: 50px;
+  margin: 10px;
+  border: 1px solid #5acfc9;
+  color: #fff;
+  font-size: 1.2rem;
+  cursor: pointer;
+}
+
+.corrects{
+    display: flex;
+    justify-content: center;
+    padding: 1rem 0.5rem;
+    font-size: 1.2rem;
+    color: #fff;
+    background: #5acfc9;
+    margin-top: 1.5rem;
+}
+.popup{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    
+    align-items: center;
+    position: fixed; 
+    z-index: 1; 
+    left: 0;
+    top: 0;
+    width: 100%; 
+    height: 100%; 
+    overflow: auto; 
+    background-color: rgb(255, 255, 255); 
+    background-color: rgba(255, 255, 255, 0.8); 
+}
+.check{
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    margin-top: 12rem;
+    color: #fff;
+    background: #8ad879;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-size: 5rem;
+    cursor: pointer;
+}
+.reset{
+    visibility: hidden;
+}
+.correct{
+    background: #8ad879;
+    visibility: visible;
+}
+.incorrect{
+    background: #f3533a;
+    visibility: visible;
+}
+.finish{
+    position: absolute;
+    background: #5acfc9;
+    visibility: visible;
+    width: 100%;
+    height: 100%;
+    color: #fff;
+    display: flex;
+    border-radius: 30px;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-size: 2.5rem;
+}
+
+
+</style>
